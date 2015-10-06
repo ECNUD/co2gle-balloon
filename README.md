dcfc62a9131dfe59bca01ea7f76482c6a0c77e7f
kommander <sebastian@formzoo.com>
2015-10-06 05:23:14 +0200

    Setup controls for volume translation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ECNUD - Awareness Device Server
 This node server controls the Awareness Device 
+
+## Setup
+Make sure the user running node is in the _dialout_ group to use the usb device (Arduino)
--- server.js
+++ server.js
@@ -1,8 +1,58 @@
 var SerialPort = require("serialport").SerialPort;
-var serialport = new SerialPort("/dev/ttyACM0");
-serialport.on('open', function(){
-  console.log('Serial Port Opend');
-  serialport.on('data', function(data){
-      console.log(data[0]);
-  });
+var serialport = new SerialPort("/dev/ttyACM0", {
+  baudrate: 57600
 });
+var http = require('http');
+var url = require('url');
+
+var useArduino = true;
+if(process.argv[2] === 'noduino'){
+  useArduino = false;
+}
+
+var buffOn = new Buffer("1");
+var buffOff = new Buffer("0");
+
+// State
+var fanRunning = false;
+
+function turnOn(){
+  if(!fanRunning) {
+    fanRunning = true;
+    serialport.write(buffOn, function(err, results) {
+      // TODO: Handle Error
+      console.log('err ' + err);
+    });
+  }
+}
+
+function turnOff(){
+  if(fanRunning) {
+    fanRunning = false;
+    serialport.write(buffOff, function(err, results) {
+      // TODO: Handle Error
+      console.log('err ' + err);
+    });
+  }
+}
+
+function setupServer(){
+  http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url);
+    if(parsedUrl.path === '/pushVolume'){
+      turnOn();
+      setTimeout(turnOff, 3000);
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('done\n');
+  }).listen(3000);
+}
+
+if(useArduino){
+  serialport.on('open', function(){
+    console.log('Serial Port Opened');
+    setupServer();
+  });
+} else {
+  setupServer();
+}
